6fad300acc56a3dc5490be25d5c42cfb7c7948ce
Ghufran <ghufranshahid101@gmail.com>
2023-05-30 20:29:15 -0400

    Added initialLoad function, which places users at the starting point of the webpage

--- src/functions/pageLoad.js
+++ src/functions/pageLoad.js
@@ -3,13 +3,43 @@
 // initial loading of index.html
 
 const container = document.getElementById('content');
+const mainContent = document.createElement('div');
 
 const initialLoad = () => {
+  const header = document.createElement('div');
+  header.classList.add('header');
 
+  const aboutBtn = document.createElement('button');
+  aboutBtn.textContent = 'About';
+  aboutBtn.id = 'aboutBtn';
+
+  const menuBtn = document.createElement('button');
+  menuBtn.textContent = 'Menu';
+  menuBtn.id = 'menuBtn';
+
+  const galleryBtn = document.createElement('button');
+  galleryBtn.textContent = 'Gallery';
+  galleryBtn.id = 'galleryBtn';
+
+  header.appendChild(aboutBtn);
+  header.appendChild(menuBtn);
+  header.appendChild(galleryBtn);
+
+  mainContent.textContent = 'Welcome to the beginning of this website, please take a look around!';
+  mainContent.classList.add('main-content');
+
+  container.appendChild(header);
+  container.appendChild(mainContent);
+  console.log('Testing');
 };
 
 // cleans up the 'container' div everytime a new page needs to be loaded
 
 const cleanPage = () => {
-  container.innerHTML = '';
+  mainContent.innerHTML = '';
+};
+
+export {
+  initialLoad,
+  cleanPage,
 };
