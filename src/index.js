0c06f4373e4b5ce0c48861ee69ce72bee0eec79f
Ghufran <ghufranshahid101@gmail.com>
2023-05-30 20:29:54 -0400

    Added imports from functions folder, assigned buttons from pageLoad values and eventlisteners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Webpack testing!');
+import './styleSheets/styles.css';
+import { initialLoad, cleanPage } from './functions/pageLoad';
+
+// begin initial setup of page
+
+initialLoad();
+
+const menuBtn = document.querySelector('#menuBtn');
+const aboutBtn = document.querySelector('#aboutBtn');
+const galleryBtn = document.querySelector('#galleryBtn');
+
+menuBtn.addEventListener('click', () => {
+  cleanPage();
+});
+
+aboutBtn.addEventListener('click', () => {
+  cleanPage();
+});
+
+galleryBtn.addEventListener('click', () => {
+  cleanPage();
+});
